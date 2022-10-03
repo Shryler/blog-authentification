84d431ab3a566fba187ec8d98d63ecdbf8deba8d
Shryler <105290336+Shryler@users.noreply.github.com>
2022-10-03 14:57:25 +0200

    Partie 1 terminée

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+function LoginForm() {
+
+    const handleFormSignInSubmit = (e) => {
+        e.preventDefault();
+        const jsonDataSignIn = Object.fromEntries(new FormData(e.target));
+        alert("Vous êtes enregistré !")
+        console.log(jsonDataSignIn);
+    }
+
+    return (
+        <form id="form1" onSubmit={handleFormSignInSubmit}  className="form-example w-50 text-center">
+            <div className="form-example d-flex flex-column w-50">
+                <label htmlFor="email">Adresse email </label>
+                <input type="email" name="email" id="email" required />
+            </div>
+            <div className="form-example d-flex flex-column w-50">
+                <label htmlFor="password">Mot de passe </label>
+                <input type="password" name="password" id="password" required />
+            </div>
+            <div className="form-example mt-2 d-flex flex-column w-50">
+                <input type="submit" value="Envoyer" />
+            </div>
+        </form>
+    );
+}
+
+export default LoginForm;
--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import LoginForm from '../components/LoginForm';
 
 function LoginScreen(props) {
     return (
         <div>
             <h1>Login</h1>
+            <LoginForm />
         </div>
     );
 }
